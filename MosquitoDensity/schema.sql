fdc2a2174149a82317ec52fe524328d744d9ecc1
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:31:45 +0200

    test of SQL export

--- MosquitoDensity/schema.sql
+++ MosquitoDensity/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model MosquitoDensity of the subject dataModel.Environment for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model MosquitoDensity of the subject dataModel.Environment for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE MosquitoDensity_type AS ENUM ('MosquitoDensity');
-CREATE TABLE MosquitoDensity (address json, airTemperature json, alternateName text, areaServed text, dataDescriptor text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, deviceInfo json, id text, location json, mosquitoDensity json, name text, observationDateTime timestamp, owner json, precipitation text, seeAlso json, source text, type MosquitoDensity_type);
+CREATE TABLE MosquitoDensity (address JSON, airTemperature JSON, alternateName TEXT, areaServed TEXT, dataDescriptor TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, deviceInfo JSON, mosquitoDensity JSON, name TEXT, observationDateTime TIMESTAMP, owner JSON, precipitation NUMERIC, source TEXT, type MosquitoDensity_type);
