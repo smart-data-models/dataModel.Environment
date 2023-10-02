de7a4e37d32e6401045934fa1e854e3a94708a21
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:32:07 +0200

    test of SQL export

--- TrafficEnvironmentImpact/schema.sql
+++ TrafficEnvironmentImpact/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model TrafficEnvironmentImpact of the subject dataModel.Environment for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model TrafficEnvironmentImpact of the subject dataModel.Environment for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE TrafficEnvironmentImpact_type AS ENUM ('TrafficEnvironmentImpact');
-CREATE TABLE TrafficEnvironmentImpact (address json, alternateName text, areaServed text, co2 text, dataProvider text, dateCreated timestamp, dateModified timestamp, dateObservedFrom timestamp, dateObservedTo timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, traffic json, type TrafficEnvironmentImpact_type);
+CREATE TABLE TrafficEnvironmentImpact (address JSON, alternateName TEXT, areaServed TEXT, co2 NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, dateObservedFrom TIMESTAMP, dateObservedTo TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, traffic JSON, type TrafficEnvironmentImpact_type);
