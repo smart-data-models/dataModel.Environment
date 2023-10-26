7bb1d3438630456e839cd475c05f5ad8f3afa5ff
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:56:43 +0200

    test of SQL export

--- AeroAllergenObserved/schema.sql
+++ AeroAllergenObserved/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model AeroAllergenObserved of the subject dataModel.Environment for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE allergenRisk_type AS ENUM ('none','low','moderate','high','veryHigh');CREATE TYPE AeroAllergenObserved_type AS ENUM ('AeroAllergenObserved');
-CREATE TABLE AeroAllergenObserved (address JSON, allergenRisk allergenRisk_type, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, dateObserved TEXT, description TEXT, name TEXT, owner JSON, source TEXT, type AeroAllergenObserved_type);
+CREATE TABLE AeroAllergenObserved (address JSON, allergenRisk allergenRisk_type, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, dateObserved TEXT, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type AeroAllergenObserved_type);
