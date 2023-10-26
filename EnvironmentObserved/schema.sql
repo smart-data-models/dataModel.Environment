4a8d1ec8876b47b24893463fcc0336c2c0b913cc
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:57:00 +0200

    test of SQL export

--- EnvironmentObserved/schema.sql
+++ EnvironmentObserved/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model EnvironmentObserved of the subject dataModel.Environment for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE EnvironmentObserved_type AS ENUM ('EnvironmentObserved');
-CREATE TABLE EnvironmentObserved (address JSON, airQualityObserved JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, pointOfInterest JSON, source TEXT, type EnvironmentObserved_type, waterQualityObserved JSON, weatherObserved JSON);
+CREATE TABLE EnvironmentObserved (address JSON, airQualityObserved JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, pointOfInterest JSON, seeAlso JSON, source TEXT, type EnvironmentObserved_type, waterQualityObserved JSON, weatherObserved JSON);
