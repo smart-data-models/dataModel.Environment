c7227fbe75c13aeab7eefcea8afc57d4b561e773
Alberto Abella <alberto.abella@fiware.org>
2023-12-14 17:37:28 +0100

    test of SQL export

--- NightSkyQuality/schema.sql
+++ NightSkyQuality/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model NightSkyQuality of the subject dataModel.Environment for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
-
-CREATE TABLE NightSkyQuality (address JSON, alternateName TEXT, ambientTemperature NUMERIC, areaServed TEXT, battery NUMERIC, batteryLevel JSON, clouds TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, sigmaMagnitude NUMERIC, skyMagnitude NUMERIC, skyTemperature NUMERIC, source TEXT);
+CREATE TYPE NightSkyQuality_type AS ENUM ('NightSkyQuality');
+CREATE TABLE NightSkyQuality (address JSON, alternateName TEXT, ambientTemperature NUMERIC, areaServed TEXT, battery NUMERIC, batteryLevel JSON, clouds TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, sigmaMagnitude NUMERIC, skyMagnitude NUMERIC, skyTemperature NUMERIC, source TEXT, type NightSkyQuality_type);
