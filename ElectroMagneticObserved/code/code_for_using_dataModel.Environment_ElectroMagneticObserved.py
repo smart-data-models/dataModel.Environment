81c9897707862dcac2b93af884e1c11c4c41c2af
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:23:43 +0200

    created example of code

--- ElectroMagneticObserved/code/code_for_using_dataModel.Environment_ElectroMagneticObserved.py
+++ ElectroMagneticObserved/code/code_for_using_dataModel.Environment_ElectroMagneticObserved.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "ElectroMagneticObserved"
 subject = "dataModel.Environment"
-dateObserved = "{'type': 'Property', 'value': {'@type': 'DateTime', '@value': '2020-03-17T08:45:00Z'}}"
+dateObserved = "2020-03-17T08:45:00Z"
 attribute = "dateObserved"
 value = dateObserved
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-eMF = {'type': 'Property', 'value': 950.12, 'observedAt': '2020-03-17T08:45:00Z', 'measurementType': {'type': 'Property', 'value': 'Instant'}, 'measurementInterval': {'type': 'Property', 'value': 1}}
+eMF = 950.12
 attribute = "eMF"
 value = eMF
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-refDevice = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:Device:NCE-T2-P7-EM03'}"
+refDevice = "urn:ngsi-ld:Device:NCE-T2-P7-EM03"
 attribute = "refDevice"
 value = refDevice
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-reliability = {'type': 'Property', 'value': 0.993}
+reliability = 0.993
 attribute = "reliability"
 value = reliability
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
