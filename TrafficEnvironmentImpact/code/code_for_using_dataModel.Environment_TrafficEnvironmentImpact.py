93fe815c78628776474af738de5189cf4d5713c8
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:24:29 +0200

    created example of code

--- TrafficEnvironmentImpact/code/code_for_using_dataModel.Environment_TrafficEnvironmentImpact.py
+++ TrafficEnvironmentImpact/code/code_for_using_dataModel.Environment_TrafficEnvironmentImpact.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "TrafficEnvironmentImpact"
 subject = "dataModel.Environment"
-dateObservedFrom = "{'type': 'Property', 'value': '2022-08-30T08:09:40Z'}"
+dateObservedFrom = "2022-08-30T08:09:40Z"
 attribute = "dateObservedFrom"
 value = dateObservedFrom
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-dateObservedTo = "{'type': 'Property', 'value': '2022-08-30T08:19:40Z'}"
+dateObservedTo = "2022-08-30T08:19:40Z"
 attribute = "dateObservedTo"
 value = dateObservedTo
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-co2 = {'type': 'Property', 'value': 582.3}
+co2 = 582.3
 attribute = "co2"
 value = co2
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-traffic = {'type': 'Property', 'value': [{'vehicleClass': 'A', 'refTrafficFlowObserved': 'urn:ngsi-ld:TrafficObserved:items:FAVE:94166126'}, {'vehicleClass': 'B', 'refTrafficFlowObserved': 'urn:ngsi-ld:TrafficObserved:items:BAAE:94166236'}]}
+traffic = [{'vehicleClass': 'A', 'refTrafficFlowObserved': 'urn:ngsi-ld:TrafficObserved:items:FAVE:94166126'}, {'vehicleClass': 'B', 'refTrafficFlowObserved': 'urn:ngsi-ld:TrafficObserved:items:BAAE:94166236'}]
 attribute = "traffic"
 value = traffic
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
