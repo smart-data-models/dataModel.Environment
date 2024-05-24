6bf1cb9539bfa1c61cffd292f65c535109b868ad
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:23:28 +0200

    created example of code

--- AeroAllergenObserved/code/code_for_using_dataModel.Environment_AeroAllergenObserved.py
+++ AeroAllergenObserved/code/code_for_using_dataModel.Environment_AeroAllergenObserved.py
@@ -24,19 +24,24 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "AeroAllergenObserved"
 subject = "dataModel.Environment"
-allergenRisk = "{'type': 'Property', 'value': 'moderate'}"
+allergenRisk = "moderate"
 attribute = "allergenRisk"
 value = allergenRisk
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-dateObserved = "{'type': 'Property', 'value': {'@type': 'DateTime', '@value': '2018-02-11T00:00:00.00Z'}}"
+dateObserved = "2018-02-11T00:00:00.00Z"
 attribute = "dateObserved"
 value = dateObserved
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
